728093c12a70b69d09c2b031bb2dbeeb21eba6f6
luzmarinapilco <luzmarinapilco@gmail.com>
2022-10-01 00:58:20 -0500

    Calculando resultados reporte

--- app.py
+++ app.py
@@ -1,114 +1,14 @@
 from __future__ import annotations
 
-from typing import List, Dict
+from typing import Counter, List, Dict
 
 from model.Airplane import Airplane
+from model.Report import Report
 from model.Route import Route
 from model.SaleTicket import SaleTicket
 from config import CURRENCY_SYMBOL
 import utils
-
-def create_list_routes() -> List[Route]:
-    # Función que crea y devuelve una lista de objetos Route
-    data_routes: List[Dict[str, str | float | int]] = [
-        {
-            "code": "LIM-AYA",
-            "name": "LIMA-AYACUCHO",
-            "base_price": 55.19,
-            "economic": 8,
-            "premium": 16,
-            "min_sales_economic": 120,
-            "max_sales_economic": 130,
-            "min_sales_premium": 10,
-            "max_sales_premium": 20
-        },
-        {
-            "code": "LIM-CUS",
-            "name": "LIMA-CUSCO",
-            "base_price": 136.51,
-            "economic": 8,
-            "premium": 16,
-            "min_sales_economic": 130,
-            "max_sales_economic": 144,
-            "min_sales_premium": 15,
-            "max_sales_premium": 24
-        },
-        {
-            "code": "LIM-ARE",
-            "name": "LIMA-AREQUIPA",
-            "base_price": 90.59,
-            "economic": 8,
-            "premium": 16,
-            "min_sales_economic": 115,
-            "max_sales_economic": 138,
-            "min_sales_premium": 16,
-            "max_sales_premium": 22
-        },
-        {
-            "code": "LIM-TAR",
-            "name": "LIMA-TARAPOTO",
-            "base_price": 71.89,
-            "economic": 8,
-            "premium": 16,
-            "min_sales_economic": 100,
-            "max_sales_economic": 120,
-            "min_sales_premium": 12,
-            "max_sales_premium": 18
-        },
-        {
-            "code": "AYA-LIM",
-            "name": "AYACUCHO-LIMA",
-            "base_price": 40.42,
-            "economic": 7,
-            "premium": 16,
-            "min_sales_economic": 100,
-            "max_sales_economic": 115,
-            "min_sales_premium": 10,
-            "max_sales_premium": 15
-        },
-        {
-            "code": "CUS-LIM",
-            "name": "CUSCO-LIMA",
-            "base_price": 124.32,
-            "economic": 7,
-            "premium": 16,
-            "min_sales_economic": 105,
-            "max_sales_economic": 120,
-            "min_sales_premium": 14,
-            "max_sales_premium": 20
-        },
-        {
-            "code": "ARE-LIM",
-            "name": "AREQUIPA-LIMA",
-            "base_price": 86.59,
-            "economic": 7,
-            "premium": 16,
-            "min_sales_economic": 100,
-            "max_sales_economic": 110,
-            "min_sales_premium": 13,
-            "max_sales_premium": 18
-        },
-        {
-            "code": "TAR-LIM",
-            "name": "TARAPOTO-LIMA",
-            "base_price": 68.42,
-            "economic": 7,
-            "premium": 16,
-            "min_sales_economic": 90,
-            "max_sales_economic": 105,
-            "min_sales_premium": 10,
-            "max_sales_premium": 15
-        }
-    ]
-
-    routes: List[Route] = []
-
-    for key, route in enumerate(data_routes):
-        obj_route = Route(str(route['code']), str(route['name']), float(route['base_price']), int(route['economic']),
-                          int(route['premium']), int(route['min_sales_economic']), int(route['max_sales_economic']),
-                          int(route['min_sales_premium']), int(route['max_sales_premium']))
-        routes.append(obj_route)
-    return routes
+import math
 
 def create_list_airplanes() -> List[Airplane]:
     # Función que crea y devuelve una lista de objetos Airplane
@@ -138,7 +38,7 @@ def create_list_airplanes() -> List[Airplane]:
             "comeback": "17:50 PM"
         }
     ]
-
+    
     airplanes: List[Airplane] = []
 
     for key, airplane in enumerate(data_plane):
@@ -147,9 +47,123 @@ def create_list_airplanes() -> List[Airplane]:
         airplanes.append(obj_airplane)
     return airplanes
 
+def create_list_routes() -> List[Route]:
+    # Crear la lista de objetos Airplane
+    airplanes: List[Airplane] = create_list_airplanes()
+
+    # Función que crea y devuelve una lista de objetos Route
+    data_routes: List[Dict[str, str | float | int | Airplane]] = [
+        {
+            "code": "LIM-AYA",
+            "name": "LIMA-AYACUCHO",
+            "base_price": 55.19,
+            "economic": 8,
+            "premium": 16,
+            "min_sales_economic": 120,
+            "max_sales_economic": 130,
+            "min_sales_premium": 10,
+            "max_sales_premium": 20,
+            "plane": Airplane(airplanes[0].name, airplanes[0].n_seat, airplanes[0].going, airplanes[0].comeback)
+        },
+        {
+            "code": "LIM-CUS",
+            "name": "LIMA-CUSCO",
+            "base_price": 136.51,
+            "economic": 8,
+            "premium": 16,
+            "min_sales_economic": 130,
+            "max_sales_economic": 144,
+            "min_sales_premium": 15,
+            "max_sales_premium": 24,
+            "plane": Airplane(airplanes[1].name, airplanes[1].n_seat, airplanes[1].going, airplanes[1].comeback)
+        },
+        {
+            "code": "LIM-ARE",
+            "name": "LIMA-AREQUIPA",
+            "base_price": 90.59,
+            "economic": 8,
+            "premium": 16,
+            "min_sales_economic": 115,
+            "max_sales_economic": 138,
+            "min_sales_premium": 16,
+            "max_sales_premium": 22,
+            "plane": Airplane(airplanes[2].name, airplanes[2].n_seat, airplanes[2].going, airplanes[2].comeback)
+        },
+        {
+            "code": "LIM-TAR",
+            "name": "LIMA-TARAPOTO",
+            "base_price": 71.89,
+            "economic": 8,
+            "premium": 16,
+            "min_sales_economic": 100,
+            "max_sales_economic": 120,
+            "min_sales_premium": 12,
+            "max_sales_premium": 18,
+            "plane": Airplane(airplanes[3].name, airplanes[3].n_seat, airplanes[3].going, airplanes[3].comeback)
+        },
+        {
+            "code": "AYA-LIM",
+            "name": "AYACUCHO-LIMA",
+            "base_price": 40.42,
+            "economic": 7,
+            "premium": 16,
+            "min_sales_economic": 100,
+            "max_sales_economic": 115,
+            "min_sales_premium": 10,
+            "max_sales_premium": 15,
+            "plane": Airplane(airplanes[0].name, airplanes[0].n_seat, airplanes[0].going, airplanes[0].comeback)
+        },
+        {
+            "code": "CUS-LIM",
+            "name": "CUSCO-LIMA",
+            "base_price": 124.32,
+            "economic": 7,
+            "premium": 16,
+            "min_sales_economic": 105,
+            "max_sales_economic": 120,
+            "min_sales_premium": 14,
+            "max_sales_premium": 20,
+            "plane": Airplane(airplanes[1].name, airplanes[1].n_seat, airplanes[1].going, airplanes[1].comeback)
+        },
+        {
+            "code": "ARE-LIM",
+            "name": "AREQUIPA-LIMA",
+            "base_price": 86.59,
+            "economic": 7,
+            "premium": 16,
+            "min_sales_economic": 100,
+            "max_sales_economic": 110,
+            "min_sales_premium": 13,
+            "max_sales_premium": 18,
+            "plane": Airplane(airplanes[2].name, airplanes[2].n_seat, airplanes[2].going, airplanes[2].comeback)
+        },
+        {
+            "code": "TAR-LIM",
+            "name": "TARAPOTO-LIMA",
+            "base_price": 68.42,
+            "economic": 7,
+            "premium": 16,
+            "min_sales_economic": 90,
+            "max_sales_economic": 105,
+            "min_sales_premium": 10,
+            "max_sales_premium": 15,
+            "plane": Airplane(airplanes[3].name, airplanes[3].n_seat, airplanes[3].going, airplanes[3].comeback)
+        }
+    ]
+
+    routes: List[Route] = []
+
+    for key, route in enumerate(data_routes):
+        obj_route = Route(str(route['code']), str(route['name']), float(route['base_price']), int(route['economic']),
+                          int(route['premium']), int(route['min_sales_economic']), int(route['max_sales_economic']),
+                          int(route['min_sales_premium']), int(route['max_sales_premium']), 
+                          Airplane(route['plane'].name, route['plane'].n_seat, route['plane'].going, route['plane'].comeback))
+        routes.append(obj_route)
+    return routes
+
 def create_sales_tickets(route: Route) -> List[SaleTicket]:
 
-    list_tickets: List[SaleTicket] = []
+    list_tickets:List[SaleTicket] =[]
     
     # Obtenemos la cantidad de ventas de asiento economico de manera aleatoria
     sales_economic: int = route.get_randon_sale_economic()
@@ -186,92 +200,90 @@ def main():
     # Crear la lista de objetos Route
     routes: List[Route] = create_list_routes()
 
-    # Crear la lista de objetos Airplane
-    airplanes: List[Airplane] = create_list_airplanes()
-
-    print("\n")
-    print("*"*30)
-    print("REPORTE DE VENTAS DEL DIA")
-    print("*"*30)
-    print("\n")
+    list_report: List[Report] = []
     
-    t_total_sales: int = 0
-    t_economic_sales: float = 0
-    t_premium_sales: float = 0
-
     for key, route in enumerate(routes):
         # creamos la lista de objetos SaleTicket (tickets de venta)
         list_sales_tickets: List[SaleTicket] = create_sales_tickets(route)
 
         # Total de tickets de venta
-        total_sales = len(list_sales_tickets)
-        t_total_sales += total_sales
+        seat_sales = len(list_sales_tickets)
+        
+        # Obteniendo numero Boletos Economicos.
+        n_economic: int = len(
+            [k for k, ticket in enumerate(list_sales_tickets) if ticket.typeTicket == "Economic"])
+        
+        # Obteniendo numero Boletos Premium.
+        n_premium: int = len([k for k, ticket in enumerate(list_sales_tickets) if ticket.typeTicket == "Premium"])
         
         # Total de ingresos por Boletos Economicos
-        economic_sales: float = round(
+        economic_sales:float = round(
             sum([ticket.total for k, ticket in enumerate(list_sales_tickets) if ticket.typeTicket == "Economic"]), 2)
-        t_economic_sales += economic_sales
-
-        # Total de ingresos por Boletos Economicos
+        
+        # Total de ingresos por Boletos Premium
         premiun_sales: float = round(
             sum([ticket.total for k, ticket in enumerate(list_sales_tickets) if ticket.typeTicket == "Premium"]), 2)
-        t_premium_sales += premiun_sales
+        
+        # Calculando la suma total de IGV cobrado
+        total_igv: float = round(
+            sum([ticket.igv for k, ticket in enumerate(list_sales_tickets)]), 2)
+        
+        # Calcular la suma total de ventas
+        total_sale = round(
+            sum([ticket.total for k, ticket in enumerate(list_sales_tickets)]), 2)
+        
+        # Guardando nuestro objeto Report
+        obj_report = Report(route.code, seat_sales, n_economic, n_premium, economic_sales, premiun_sales,
+                           total_igv, total_sale, route.airplane.name)
+        
+        list_report.append(obj_report)
+        
+    """ ticket_avg_economic: float = round(economic_sales / n_economic, 2)
+        t_n_economic += n_economic
+        
+        # Calculando el valor del ticket promedio de un Boleto Premium.
+        n_premium: int = len([k for k, ticket in enumerate(list_sales_tickets) if ticket.typeTicket == "Premium"])
+        ticket_avg_premium: float = round(premiun_sales / n_premium, 2)
+        t_n_premium += n_premium
 
         # Calcular la suma total de ventas
         rep_total_sale = round(
             sum([ticket.total for k, ticket in enumerate(list_sales_tickets)]), 2)
-
-        # Calculando la suma total de IGV cobrado
-        rep_total_igv: float = round(
-            sum([ticket.igv for k, ticket in enumerate(list_sales_tickets)]), 2)
-
-        # Calculando el valor del ticket promedio de venta.
-        ticket_avg: float = round(rep_total_sale / total_sales, 2)
-
-        # Ordenemos lista de tickets (de menor a mayor)
-        sorted_tickets: List[SaleTicket] = sorted(
-            list_sales_tickets, key=lambda x: x.total)
-
-        # Obtenemos la cantidad de tickets cuyo importe supera el ticket promedio
-        rep_tickets_higher_than: int = len(
-            [k for k, ticket in enumerate(sorted_tickets) if ticket.total > ticket_avg])
-
-
-        # Mostramos en pantalla valores calculados.
-        print(f"Ruta: {route.name}")
-        print(f"Cod. Ruta: {route.code}")
-        print(f"Ventas: {total_sales}")
-        print(
-            f"Total de Ventas: {utils.get_currency_format(CURRENCY_SYMBOL,rep_total_sale)}")
-        
-        print(
-            f"Total de Ventas Boletos economico: {utils.get_currency_format(CURRENCY_SYMBOL,economic_sales)}")
-
-        print(
-            f"Total de Ventas Boletos premium: {utils.get_currency_format(CURRENCY_SYMBOL, premiun_sales)}")
-
-        print(
-            f"Total de IGV: {utils.get_currency_format(CURRENCY_SYMBOL, rep_total_igv)}")
-        print(
-            f"Ticket Promedio: {utils.get_currency_format(CURRENCY_SYMBOL, ticket_avg)}")
-        print(
-            f"Ticket de Venta más baja: {utils.get_currency_format(CURRENCY_SYMBOL, sorted_tickets[0].total)}")
-        print(
-            f"Ticket de Venta más alta: {utils.get_currency_format(CURRENCY_SYMBOL, sorted_tickets[-1].total)}")
-        print(
-            f"Total de Tickets mayor al promedio de venta: {rep_tickets_higher_than}")
-        print("\n")
-        print("-"*30)
-        print("\n")
-
-        
-        
-    print(f"Total de pasajes vendidos : {t_total_sales}")
-    print(
-          f"Total de Boletos Economicos: {utils.get_currency_format(CURRENCY_SYMBOL, t_economic_sales)}")
-    print(
-        f"Total de Boletos Premium: {utils.get_currency_format(CURRENCY_SYMBOL, t_premium_sales)}")
+    """
+    seat_sales: int = sum(
+        [report.n_seat_sales for k, report in enumerate(list_report)])
     
+    economic_sales: float = sum(
+        [report.t_economic_sales for k, report in enumerate(list_report)])
+    
+    premium_sales: float = sum(
+        [report.t_premium_sales for k, report in enumerate(list_report)])
+    
+    n_economic: int = sum(
+        [report.n_seat_economic for k, report in enumerate(list_report)])
+    
+    n_premium: int = sum(
+        [report.n_seat_premium for k, report in enumerate(list_report)])
+    
+    total_igv: float = sum(
+        [report.t_igv_sales for k, report in enumerate(list_report)])
+        
+    print("\n")
+    print("*"*30)
+    print("REPORTE DE VENTAS DEL DIA")
+    print("*"*30)
+    print("\n")
+    print(f"Total de pasajes vendidos : {seat_sales}")
+    print(
+        f"Total de Boletos Economicos: {utils.get_currency_format(CURRENCY_SYMBOL, economic_sales)}")
+    print(
+        f"Total de Boletos Premium: {utils.get_currency_format(CURRENCY_SYMBOL, premium_sales)}")
+    print(
+        f"Total de IGV: {utils.get_currency_format(CURRENCY_SYMBOL, total_igv)}")
+    print(
+        f"Valor Promedio Pasaje Economico: {utils.get_currency_format(CURRENCY_SYMBOL, round(economic_sales / n_economic, 2))}")
+    print(
+        f"Valor Promedio Pasaje Premium: {utils.get_currency_format(CURRENCY_SYMBOL, round(premium_sales / n_economic, 2))}")
     print("\n")
     print("-"*30)
     print("\n")
